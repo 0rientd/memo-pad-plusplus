eb279101a2dbb515d33d1ca041825312ae8ff201
Carlos Henrique <carlosbarros@unimedrj.coop.br>
2026-06-15 16:53:52 -0300

    criado render de sombras do botao

--- src/ui/button.cpp
+++ src/ui/button.cpp
@@ -27,6 +27,18 @@ void renderButtonBorder(SDL_Renderer* renderer, int x, int y, int h, int w) {
   SDL_RenderDrawLine(renderer, x + w, y, x + w, y + h);
 }
 
+void renderButtonShadow(SDL_Renderer* renderer, int x, int y, int h, int w) {
+  SDL_SetRenderDrawColor(renderer, 34, 34, 34, 150);
+  
+  for (int i = 0; i < 3; i++) {
+    // left line
+    SDL_RenderDrawLine(renderer, x - 2 - i, y + i, x - 2 - i, y + h + i);
+
+    // bottom line
+    SDL_RenderDrawLine(renderer, x - 2 - i, y + h + i, x + w - 2, y + h + i);
+  }
+}
+
 int renderButton(SDL_Renderer* renderer, TTF_Font* font, const char* text, int x, int y, int h, int w) {
   int text_width = 0, text_height = 0;
 
@@ -38,6 +50,7 @@ int renderButton(SDL_Renderer* renderer, TTF_Font* font, const char* text, int x
   r.w = w;
   SDL_RenderFillRect(renderer, &r);
   renderButtonBorder(renderer, x, y, h, w);
+  renderButtonShadow(renderer, x, y, h, w);
 
   TTF_SizeText(font, text, &text_width, &text_height);
   renderTextInput(renderer, font, text, position_x(x, w, text_width), position_y(y, h, text_height));
